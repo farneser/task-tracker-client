4f5ebc18807417c03d01c37c2ecb8c1a2eda5ca9
farneser <farneser1@gmail.com>
2023-11-13 01:32:54 +0300

    feat: error message and auth service refresh method

---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import constants from "@/src/constants";
+import {Token} from "@/src/models/token";
+import axios from "axios";
+import {getContentType} from "@/src/api/api.helper";
+import {ErrorMessage} from "@/src/models/errorMessage";
+import {setLocalStorage} from "@/src/utils/localStorage.utils";
+
+export const authService = {
+    async refreshToken(token: Token): Promise<Token> {
+
+        const response = await axios.post<Token | ErrorMessage>(`${constants.baseUrl}/api/v1/auth/refresh`, token, {
+            headers: getContentType()
+        });
+
+        if ((response.data as Token).access_token) {
+            const data = response.data as Token;
+
+            setLocalStorage(constants.authTokenKey, data)
+        }
+
+        return token;
+    }
+}
+
+export default authService;
