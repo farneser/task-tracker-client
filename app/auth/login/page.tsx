33c970bb3ac92cc607c1ce5c31e525729ddcf3b4
farneser <farneser1@gmail.com>
2023-11-14 03:14:54 +0300

    feat: scss declaration

--- app/auth/login/page.tsx
+++
@@ -1,8 +0,0 @@
-import {NextPage} from 'next';
-import LoginPage from "@/src/components/pages/auth/login/LoginPage";
-
-const Login: NextPage = () => {
-    return <LoginPage/>;
-};
-
-export default Login;
