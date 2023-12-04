9485ef01123b716274243b102f3ac7aa0390716b
farneser <farneser1@gmail.com>
2023-12-04 05:26:19 +0300

    feat: use tasks service hook

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,13 +1,21 @@
-import {FC, useEffect} from "react";
-import {columnService} from "@/services/column/column.service.ts";
+import {FC} from "react";
+import useColumnService from "@/hooks/useColumnService.ts";
+import Column from "@/components/ui/column/Column.tsx";
 
 const HomePage: FC = () => {
-    useEffect(() => {
-        console.log("start loading")
-        columnService.get().then(data => console.log(data));
-    }, []);
+    const columnService = useColumnService()
 
-    return <>homepage</>
+    return <div>
+        <div>
+            <h1>controls</h1>
+
+        </div>
+        <div>
+            <h1>columns</h1>
+            {columnService.columns.map(column => <Column column={column} key={column.id}/>)}
+        </div>
+    </div>
+
 };
 
 export default HomePage;
