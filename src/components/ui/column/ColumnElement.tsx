c81f3dd81023c5e6951ad90138aaf859368f46cf
farneser <farneser1@gmail.com>
2023-12-07 05:45:42 +0300

    feat: create, update, delete tasks

--- src/components/ui/column/ColumnElement.tsx
+++ src/components/ui/column/ColumnElement.tsx
@@ -3,6 +3,11 @@ import {ColumnView, PatchColumnDto} from "@/services/column/column.types.ts";
 import usePopup from "@/hooks/usePopup.tsx";
 import {columnService} from "@/services/column/column.service.ts";
 import PatchColumnForm from "@/components/ui/column/patch/PatchColumnForm.tsx";
+import {CreateTaskDto, TaskView} from "@/services/task/task.types.ts";
+import CreateTaskForm from "@/components/ui/task/create/CreateTaskForm.tsx";
+import {taskService} from "@/services/task/task.service.ts";
+import TaskElement from "@/components/ui/task/TaskElement.tsx";
+import useColumnService from "@/hooks/useColumnService.ts";
 
 type ColumnProps = {
     column: ColumnView;
@@ -11,29 +16,53 @@ type ColumnProps = {
 }
 
 const ColumnElement: FC<ColumnProps> = ({column, deleteColumn, updateColumn}) => {
-    const {reversePopup, closePopup, Popup} = usePopup()
+    const {reversePopup: reverseEditPopup, closePopup: closeEditPopup, Popup: EditPopup} = usePopup()
+    const {reversePopup: reverseCreatePopup, closePopup: closeCreatePopup, Popup: CreatePopup} = usePopup()
+    const {updateTask} = useColumnService()
 
-    const onSubmit = async (data: PatchColumnDto) => {
-        if (data.columnName.trim() !== "") {
-            const dto = await columnService.patch(column.id, data);
+    const onEditSubmit = async (data: PatchColumnDto) => {
+        const dto = await columnService.patch(column.id, data);
 
-            if (updateColumn) {
-                updateColumn(dto)
-            }
-
-            closePopup();
+        if (updateColumn) {
+            updateColumn(dto)
         }
+
+        closeEditPopup();
     };
+
+    const onCreateSubmit = async (data: CreateTaskDto) => {
+        const dto = await taskService.create(data);
+
+        let tasks = column.tasks || [];
+
+        tasks = tasks.filter(task => task.id !== dto.id)
+
+        if (updateColumn) {
+            updateColumn({...column, tasks: [...tasks, dto]})
+        }
+
+        closeCreatePopup();
+    };
+    const updateTaskHandler = async (data: TaskView) => {
+        await updateTask(data)
+    }
 
     return <div>
         <h1>{column.columnName}</h1>
+
+        <button onClick={reverseCreatePopup}>Create New Task</button>
+
+        <CreatePopup>
+            <CreateTaskForm onSubmit={onCreateSubmit} columnId={column.id}/>
+        </CreatePopup>
+
         {updateColumn &&
             <>
-                <button onClick={reversePopup}>Edit</button>
+                <button onClick={reverseEditPopup}>Edit</button>
 
-                <Popup>
-                    <PatchColumnForm onSubmit={onSubmit} column={column}/>
-                </Popup>
+                <EditPopup>
+                    <PatchColumnForm onSubmit={onEditSubmit} column={column}/>
+                </EditPopup>
             </>
         }
         {deleteColumn &&
@@ -42,11 +71,22 @@ const ColumnElement: FC<ColumnProps> = ({column, deleteColumn, updateColumn}) =>
             </div>
         }
         <div>
-            <ul>
-                <li>{column.isCompleted ? 'Completed' : 'Not Completed'}</li>
-                <li>{column.orderNumber}</li>
-                <li>{column.tasks?.length}</li>
-            </ul>
+            {column.tasks?.map(task =>
+                <TaskElement
+                    key={task.id}
+                    task={task}
+                    updateTask={updateTaskHandler}
+                    deleteTask={() => {
+                        taskService.delete(task.id).then(() => {
+                            let tasks = column.tasks || [];
+
+                            tasks = tasks.filter(task => task.id !== task.id)
+
+                            if (updateColumn) {
+                                updateColumn({...column, tasks})
+                            }
+                        })
+                    }}/>)}
         </div>
     </div>
 }
