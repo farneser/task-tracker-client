eb5cd770620589360caab6b67633bf2087ac5d07
farneser <farneser1@gmail.com>
2023-12-12 20:00:04 +0300

    feat: footer and layout

--- src/components/pages/RootPage.tsx
+++ src/components/pages/RootPage.tsx
@@ -196,7 +196,6 @@ const RootPage: FC = () => {
                         }}/>
                     )}
                 </DragOverlay>, document.body)}
-
             </DndContext>
         </div>
     );
--- src/components/ui/layout/default/Layout.tsx
+++ src/components/ui/layout/default/Layout.tsx
@@ -1,8 +1,9 @@
 import {FC} from "react";
 import {Outlet} from "react-router-dom";
+import styles from "./Layout.module.scss";
 
 const Layout: FC = () => {
-    return <div>
+    return <div className={styles.layout__container}>
         <Outlet/>
     </div>
 }
