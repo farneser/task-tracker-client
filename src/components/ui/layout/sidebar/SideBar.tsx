bf917db4ee0edf6a60701c9e79a048cd95316f9f
farneser <farneser1@gmail.com>
2024-05-05 23:05:38 +0300

    feat: sidebar button styles

--- src/components/ui/layout/sidebar/SideBar.tsx
+++ src/components/ui/layout/sidebar/SideBar.tsx
@@ -1,12 +1,32 @@
 import {FC} from "react";
 import useProjects from "@/hooks/useProjects.ts";
 import {Link} from "react-router-dom";
+import styles from "./SideBar.module.scss";
+import usePopup from "@/hooks/usePopup.tsx";
 
 const SideBar: FC = () => {
     const {projects} = useProjects();
 
-    return <div>
-        <ul>{projects.map(p => (<li><Link to={`p/${p.id}`}>{p.projectName}</Link></li>))}</ul>
+    const {openPopup, Popup} = usePopup();
+
+    return <div className={styles.sidebar__container}>
+        <Popup>
+            <div>help</div>
+        </Popup>
+        <div className={styles.sidebar__container__head}>
+            <ul>
+                <li><Link to="p">Dashboard</Link></li>
+                <li><Link to="#" onClick={openPopup}>Create new Project</Link></li>
+            </ul>
+        </div>
+        <div className={styles.sidebar__container__sepatator}/>
+        <div className={styles.sidebar__container__list}>
+            <ul>
+                {projects.map(p => (
+                    <li><Link to={`p/${p.id}`}>{p.projectName}</Link></li>
+                ))}
+            </ul>
+        </div>
     </div>
 }
 
