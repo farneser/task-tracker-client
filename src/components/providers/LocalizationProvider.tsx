7dad26678def2b67e4a8ae58aceccca02f8042b7
farneser <farneser1@gmail.com>
2024-05-06 19:28:58 +0300

    feat: store locate in localstorage

--- src/components/providers/LocalizationProvider.tsx
+++ src/components/providers/LocalizationProvider.tsx
@@ -1,5 +1,6 @@
 import locales, {LocaleKey, Translations} from "@/hooks/locals/locales.ts";
-import {createContext, FC, PropsWithChildren, useState} from "react";
+import {createContext, FC, PropsWithChildren, useEffect, useState} from "react";
+import {getLocalStorageItem, setLocalStorage} from "@/utils/localStorage.utils.ts";
 
 interface LocaleContextType {
     locale: LocaleKey;
@@ -10,13 +11,27 @@ interface LocaleContextType {
 export const LocalizationContext = createContext<LocaleContextType | undefined>(undefined);
 
 const LocalizationProvider: FC<PropsWithChildren> = ({children}) => {
-    const [locale, setLocale] = useState<LocaleKey>('en');
+    const [locale, setLocaleState] = useState<LocaleKey>(() => {
+        return getLocalStorageItem<LocaleKey>('locale') || "en";
+    });
+
+    const setLocale = (newLocale: LocaleKey) => {
+        setLocaleState(newLocale);
+
+        setLocalStorage('locale', newLocale)
+    };
 
     const contextValue: LocaleContextType = {
         locale,
         translations: locales[locale],
         setLocale,
     };
+
+    useEffect(() => {
+        const storedLocale = getLocalStorageItem<LocaleKey>('locale');
+
+        setLocaleState(storedLocale ?? "en");
+    }, [locale]);
 
     return (
         <LocalizationContext.Provider value={contextValue}>
