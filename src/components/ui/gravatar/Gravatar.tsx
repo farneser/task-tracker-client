a0a507b674407e9353b6014ea05e84d04754b8d1
farneser <farneser1@gmail.com>
2024-05-11 16:25:53 +0300

    chore: extend gravatar component props

--- src/components/ui/gravatar/Gravatar.tsx
+++ src/components/ui/gravatar/Gravatar.tsx
@@ -5,17 +5,28 @@ interface GravatarProps {
     email: string;
     size?: number;
     className?: string;
+    borderRadius?: string;
+    onClick?: () => void;
 }
 
-const Gravatar: React.FC<GravatarProps> = ({email, size = 200, className = ''}) => {
+const Gravatar: React.FC<GravatarProps> = ({email, size = 200, className = '', borderRadius = '50%', onClick}) => {
     const gravatarUrl = `https://www.gravatar.com/avatar/${md5(email)}?s=${size}`;
 
+    const handleClick = () => {
+        if (onClick) {
+            onClick();
+        } else {
+            window.open("https://gravatar.com", '_blank');
+        }
+    };
+
     return (
-        <img onClick={() => window.open("https://gravatar.com", '_blank')}
-             src={gravatarUrl}
-             alt="gravatar.com"
-             className={`gravatar ${className}`}
-             style={{borderRadius: '50%', width: size, height: size, cursor: "pointer"}}
+        <img
+            onClick={handleClick}
+            src={gravatarUrl}
+            alt="gravatar.com"
+            className={`gravatar ${className}`}
+            style={{borderRadius, width: size, height: size, cursor: "pointer"}}
         />
     );
 };
